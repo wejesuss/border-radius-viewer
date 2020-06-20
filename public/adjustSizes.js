3a9e53f1bb3a49c2d83887a2e070c371fb7edb09
Wemerson Jesus <wejesuss@gmail.com>
2020-06-20 16:30:50 -0300

    Adjust initial input value using the client's screen size

--- public/adjustSizes.js
+++ public/adjustSizes.js
@@ -1,7 +1,17 @@
 const width = document.getElementById("width")
 const height = document.getElementById("height")
 
+if(window.innerWidth > 700) {
+    width.value = 310
+    height.value = 310
+}
+
 if(window.innerWidth >= 1600) {
     width.value = 450
     height.value = 450
+}
+
+if(window.innerWidth < 700) {
+    width.value = 280
+    height.value = 280
 }
