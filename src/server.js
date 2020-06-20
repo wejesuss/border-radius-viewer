d1b3946356518feed4a6b5749291f3ba06f58d70
Wemerson Jesus <wejesuss@gmail.com>
2020-06-20 19:59:18 -0300

    Add heroku website of the application

--- src/server.js
+++ src/server.js
@@ -7,6 +7,6 @@ app.get("/", (req, res) => {
     return res.sendFile(__dirname + "/views/index.html")
 })
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log("I am here")
 })
